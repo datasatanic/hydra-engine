bf16b6e0c49cfeb3859e7a3a1b476571eafb7f1c
Mdryomin <mdryomin@bk.ru>
2022-12-09 18:31:38 +0500

    change structure of json scheme

--- hydra-engine/app.py
+++ hydra-engine/app.py
@@ -1,29 +1,29 @@
-from schemas import add_node, tree
+from schemas import add_node, tree, add_additional_fields
 from fastapi.encoders import jsonable_encoder
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
 
-def read_file():
+def read_file(file_name: str):
     tree.clear()
-    f = open('controls.meta')
+    path = ""
+    f = open(file_name)
     for line in f:
-        str_list = list(filter(lambda x: len(x), line.replace('\n', '').split(":")))
-        if len(str_list) == 1 and line != '\n':
-            print(line.replace('\n', "").split(":"))
-            add_node(str_list[0].split("/"))
+        str_list = list(filter(lambda x: len(x), line.replace('\n', '').strip().split(":")))
+        if line != '\n':
+            if len(str_list) == 1:
+                path = str_list[0]
+                add_node(path.split("/"))
+            else:
+                add_additional_fields(path.split("/"), str_list)
 
 
-@app.get("/")
-def get_tree():
-    read_file()
-    print_tree(tree)
-    return {"tree": jsonable_encoder(tree)}
-
-
-def print_tree(dir_tree):
-    for item in dir_tree:
-        print("---" * item.level, item.name)
-        if len(item.child_items) > 0:
-            print_tree(item.child_items)
+@app.get("/tree")
+def get_tree(file_name: str):
+    try:
+        read_file(file_name)
+        return JSONResponse(content=jsonable_encoder(tree), status_code=200)
+    except FileNotFoundError:
+        return JSONResponse(content={"detail": "File or directory not found"}, status_code=400)
--- hydra-engine/schemas.py
+++ hydra-engine/schemas.py
@@ -1,22 +1,26 @@
 from typing import List
 import json
-from pydantic import BaseModel
 
-tree_dict = {}
-tree = []
+import pydantic
+from pydantic import BaseModel, validator, Extra
+
+tree = {}
 
 
 class Node(BaseModel):
-    name: str
-    level: int
-    child_items: List | None = None
+    child: dict = {}
+    elem: List = []
+
+    class Config:
+        extra = Extra.allow
 
 
 def add_node(_list):
     level = len(_list)
     if level == 1:
-        node = Node(name=_list[0], child_items=[], level=1)
-        tree.append(node)
+        node = Node(elem=[], child={})
+        d = {_list[0]: node}
+        tree.update(d)
     else:
         add_node_subtree(tree, 0, _list)
 
@@ -24,11 +28,30 @@ def add_node(_list):
 def add_node_subtree(subtree, j, _list):
     n = len(subtree)
     for i in range(0, n):
-        if subtree[i].name == _list[j]:
+        if _list[j] in subtree:
+            subtree = subtree[_list[j]].child
             j += 1
-            subtree = subtree[i].child_items
             if j == len(_list) - 1:
-                node = Node(name=_list[j], child_items=[], level=j + 1)
-                subtree.append(node)
+                node = Node(elem=[], child={})
+                d = {_list[j]: node}
+                if len([x for x in subtree if _list[j] in x]):
+                    raise ValueError("Not valid file")
+                subtree.update(d)
                 return
             add_node_subtree(subtree, j, _list)
+
+
+def add_additional_fields(node_list, field_list):
+    node = find_node(node_list)
+    if field_list[0] in node.__dict__:
+        raise ValueError("Not valid file")
+    node.__dict__[field_list[0]] = field_list[1].replace('"', '').strip()
+
+
+def find_node(node_list):
+    subtree = tree
+    find = {}
+    for node in node_list:
+        find = subtree[node] if node in subtree else find
+        subtree = find.child
+    return find
