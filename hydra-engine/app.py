c4fa32b5e1489e6f80eed663459e329b0e63fa0b
Mdryomin <mdryomin@bk.ru>
2022-12-12 07:11:09 +0500

    add filtering request (only forms are submitted to the client)

--- hydra-engine/app.py
+++ hydra-engine/app.py
@@ -2,8 +2,22 @@ from schemas import add_node, tree, add_additional_fields
 from fastapi.encoders import jsonable_encoder
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "https://localhost:7285"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 def read_file(file_name: str):
@@ -20,10 +34,22 @@ def read_file(file_name: str):
                 add_additional_fields(path.split("/"), str_list)
 
 
+def filter_tree(all_tree):
+    tree_filter = all_tree
+    keys = list(tree_filter)
+    for key in keys:
+        if len(tree_filter[key].child) == 0:
+            tree_filter.pop(key)
+        else:
+            filter_tree(tree_filter[key].child)
+    return tree_filter
+
+
 @app.get("/tree")
 def get_tree(file_name: str):
     try:
         read_file(file_name)
-        return JSONResponse(content=jsonable_encoder(tree), status_code=200)
+        forms = filter_tree(tree)
+        return JSONResponse(content=jsonable_encoder(forms), status_code=200)
     except FileNotFoundError:
         return JSONResponse(content={"detail": "File or directory not found"}, status_code=400)
