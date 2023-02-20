08230223d992c47c90d58db24244d179ad5ed729
Mdryomin <mdryomin@bk.ru>
2023-02-20 18:35:35 +0500

    commit for home

--- hydra_engine/__main__.py
+++ hydra_engine/__main__.py
@@ -2,6 +2,7 @@ import logging
 from hydra_engine._logging import global_logging_config
 
 from app import app
+from validator import validator
 import uvicorn
 from filewatcher import start_monitoring_files
 logger = logging.getLogger("common_logger")
@@ -10,3 +11,4 @@ logger = logging.getLogger("common_logger")
 if __name__ == "__main__":
     start_monitoring_files()
     uvicorn.run(app, host="127.0.0.1", log_config=global_logging_config, port=8000)
+    validator()
--- hydra_engine/validator.py
+++ hydra_engine/validator.py
@@ -14,9 +14,9 @@ def validate_node(current_node):
     keys = current_node.keys()
     for key in keys:
         for elem in current_node[key].elem:
-            elem[elem.keys()[0]].check_type()
-            elem[elem.keys()[0]].check_sub_type()
-            elem[elem.keys()[0]].check_control()
-            elem[elem.keys()[0]].check_constraints(
+            elem[list(elem.keys())[0]].check_type()
+            elem[list(elem.keys())[0]].check_sub_type()
+            elem[list(elem.keys())[0]].check_control()
+            elem[list(elem.keys())[0]].check_constraints(
             )
         validate_node(current_node[key].child)
