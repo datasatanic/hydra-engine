3416de6a43eb0d227865dd5c0c9c3befd90fbb34
Mdryomin <mdryomin@bk.ru>
2023-02-22 14:37:12 +0500

    fix cli validator

--- hydra_engine/validator.py
+++ hydra_engine/validator.py
@@ -1,21 +1,11 @@
 import typer
-from app import tree, read_controls_file
+from app import read_controls_file, parse_config_files
+from schemas import ElemInfo
 
 validator = typer.Typer()
 
 
 @validator.command()
 def validate():
-    validate_node(tree)
-
-
-def validate_node(current_node):
-    keys = list(current_node)
-    for key in keys:
-        for elem in current_node[key].elem:
-            values = elem[list(elem.keys())[0]]
-            elem[list(elem.keys())[0]].check_type(values.type, values.__dict__)
-            elem[list(elem.keys())[0]].check_sub_type(values.sub_type, values.__dict__)
-            elem[list(elem.keys())[0]].check_control(values.control, values.__dict__)
-            elem[list(elem.keys())[0]].check_constraints(values.constraints, values.__dict__)
-        validate_node(current_node[key].child)
+    parse_config_files()
+    read_controls_file("files/controls.meta")
