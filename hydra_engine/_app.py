3f1e4ab08588c7e3083e3a6d58388c9a5009eb59
Mdryomin <mdryomin@bk.ru>
2023-05-15 22:07:06 +0500

    add highlight for differences in terraform plan(resource_changes)

--- hydra_engine/_app.py
+++ hydra_engine/_app.py
@@ -70,7 +70,7 @@ async def set_values(request: Request):
                 with open(os.path.join(root, name)) as file:
                     plan = json.load(file)
     return templates.TemplateResponse("plan.html",
-                                      {"request": request, "plan": {"plan": plan},
+                                      {"request": request, "plan": {"plan": find_changes_in_terraform_plan(plan)},
                                        "id": uuid})
 
 
@@ -99,3 +99,44 @@ def read_controls_file(directory):
                 except Exception as e:
                     logger.error(f"Error in parsing meta file of tree {e}")
                     raise ValueError("Error in parsing meta file of tree {e}")
+
+
+def find_changes_in_terraform_plan(plan_dict):
+    resource_changes = plan_dict["resource_changes"]
+    changes_dict = {}
+    for resource in resource_changes:
+        resource_address = resource["address"]
+        changes_dict[resource_address] = {"old": {}, "new": {}}
+        change_after = resource.get("change").get("after")
+        change_after_unknown = resource.get("change").get("after_unknown")
+        change_before = resource.get("change").get("before")
+        change_actions = resource.get("change").get("actions")
+        if "no-op" not in change_actions:
+            for key, value in change_before.items():
+                new_value = None
+                old_value = value
+                if key in change_after:
+                    new_value = change_after.get(key)
+                if key not in change_after and key in change_after_unknown:
+                    changes_dict[resource_address]["old"][key] = old_value
+                    changes_dict[resource_address]["new"][key] = "known after apply"
+                elif old_value != new_value and (
+                        new_value is not None and new_value != "" or old_value is not None and old_value != ""):
+                    changes_dict[resource_address]["old"][key] = old_value
+                    changes_dict[resource_address]["new"][key] = new_value
+            for key, value in change_after.items():
+                if key not in change_before:
+                    new_value = value
+                    changes_dict[resource_address]["new"][key] = new_value
+        else:
+            for key, value in change_before.items():
+                changes_dict[resource_address]["old"][key] = value
+        resource[resource_address] = {"actions": change_actions, "values_change": changes_dict[resource_address]}
+        resource.pop("change")
+        resource.pop("address")
+        for key in list(resource):
+            if key != resource_address:
+                resource[resource_address][key] = resource[key]
+                resource.pop(key)
+    return plan_dict
+
---
+++ hydra_engine/static/script.js
@@ -0,0 +1,42 @@
+﻿function highlightDifferences(element1, element2) {
+    // Если оба элемента существуют
+    if (element1 && element2) {
+        // Сравнить значения элементов
+        if (element1.textContent !== element2.textContent) {
+            if (element1.tagName === "H5" && element2.tagName === "H5"){
+                element1.style.color = 'red';
+                element2.style.color = 'green';
+            }
+            else if(element1.tagName!=="H5" && element2.tagName === "H5"){
+                element1.style.color = 'red';
+                element2.style.color = 'green';
+            }
+            else if (element1.tagName==="H5" && element2.tagName !== "H5"){
+                element1.style.color = 'red';
+                element2.style.color = 'green';
+            }
+        }
+
+        // Рекурсивно проверить дочерние элементы
+        const children1 = element1.children;
+        const children2 = element2.children;
+        for (let i = 0; i < children1.length; i++) {
+            highlightDifferences(children1[i], children2[i]);
+        }
+    }
+    // Если только первый элемент существует
+    else if (element1) {
+        element1.style.color = 'red';
+        
+    }
+    // Если только второй элемент существует
+    else if (element2) {
+        element2.style.color = 'green';
+        
+    }
+}
+window.onload=function (){
+    let old_values=document.querySelector(".old-values>.resource-changes-container")
+    let new_values=document.querySelector(".new-values>.resource-changes-container")
+    highlightDifferences(old_values,new_values)
+}
