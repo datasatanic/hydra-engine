4e2e691a58f75faf7eefeb2eaf4df4f053c6cc5d
Mdryomin <mdryomin@bk.ru>
2023-05-24 23:51:38 +0500

    fix design, add Modal window to confirm reset configs

---
+++ hydra_engine_blazor/Pages/ResetModel.cs
@@ -0,0 +1,7 @@
+﻿namespace DefaultNamespace;
+
+public class ResetModel
+{
+    public string Title { get; set; } = "Reset configuration";
+    public string Content { get; set; } = "All your changes will be reset to the state from the Git repository";
+}
