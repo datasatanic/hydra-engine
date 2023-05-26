505583fe017d32d7255bcbad582a7903eeaece7f
Mdryomin <mdryomin@bk.ru>
2023-05-26 21:10:34 +0500

    fix slider in client,fix design in 992px to 1200px

--- hydra_engine_blazor/Shared/MainLayout.razor.js
+++ hydra_engine_blazor/Shared/MainLayout.razor.js
@@ -1,8 +1,6 @@
 export function ResizeMenu(x,element){
     if(x>=460 && x<=window.innerWidth*0.7){
         element.style.width=x+"px";
-        const search=document.getElementById("tree-search");
-        search.style.width=x-160+"px";
     }
     
 }
