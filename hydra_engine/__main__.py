7f2f61a3760a5754f72068684a4fa38393b98b2b
Mdryomin <mdryomin@bk.ru>
2023-12-19 17:13:59 +0500

    disabled save button, when one of element is not valid

--- hydra_engine/__main__.py
+++ hydra_engine/__main__.py
@@ -1,4 +1,6 @@
 import logging
+import threading
+
 from hydra_engine._logging import global_logging_config
 from hydra_engine import app_static, start_monitoring_files
 import uvicorn
@@ -6,5 +8,15 @@ import uvicorn
 logger = logging.getLogger("common_logger")
 
 if __name__ == "__main__":
-    start_monitoring_files()
+    monitor_lock = threading.Lock()
+
+
+    def start_monitoring_with_lock():
+        with monitor_lock:
+            start_monitoring_files()
+
+
+    monitoring_thread = threading.Thread(target=start_monitoring_with_lock)
+    monitoring_thread.start()
     uvicorn.run(app_static, host="127.0.0.1", log_config=global_logging_config, port=8080)
+    monitoring_thread.join()
