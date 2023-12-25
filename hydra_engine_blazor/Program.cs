6e5ce88706c9d592ad8f058c91c70cb85e2353f3
Mdryomin <mdryomin@bk.ru>
2023-12-25 07:21:11 +0500

    add dev config

--- hydra_engine_blazor/Program.cs
+++ hydra_engine_blazor/Program.cs
@@ -7,6 +7,15 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+var _client = new HttpClient
+{
+    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+};
+
+if (builder.HostEnvironment.IsDevelopment())
+    builder.Configuration.AddJsonStream(await _client.GetStreamAsync("/configs/appsettings.Development.json"));
+
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services
     .AddHttpClient("WebApi",
